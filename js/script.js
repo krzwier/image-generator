12bb43cb2f1361185959fedf340026e97d38d2da
krzwier <krzwier@gmail.com>
2021-05-02 14:00:30 -0500

    Add retrieve function for Lorem Picsum API

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-const button = document.querySelector("button");
-const authorSpan = document.querySelector(".author");
+
+pan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+const getImage = async function () {
+    const response = await fetch("https://picsum.photos/v2/list?limit=100");
+    const images = await response.json();
+    console.log(images);
+};
+
+getImage();

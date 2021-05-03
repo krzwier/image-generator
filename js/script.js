e34e0c0d424b88cd48ca7a176aa6f8ab4e34b499
krzwier <krzwier@gmail.com>
2021-05-03 11:14:00 -0500

    Add button to show random image

--- js/script.js
+++ js/script.js
@@ -1,12 +1,31 @@
 
-pan = document.querySelector(".author");
+const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+const button = document.querySelector("button");
 
 const getImage = async function () {
     const response = await fetch("https://picsum.photos/v2/list?limit=100");
     const images = await response.json();
-    console.log(images);
+    selectRandomImage(images);
 };
 
-getImage();
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    console.log(randomImage);
+    displayImage(randomImage);
+};
+
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    imageAddress = randomImage.download_url;
+    console.log(imageAddress);
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+button.addEventListener("click", function () {
+    getImage();
+});
